97d4ebf74f133edc37021e9708d5c1aaa80d9a8c
leemhyungyu <lhglbkksj@naver.com>
2022-11-03 13:32:15 +0900

    setp 2 - viewModel 적용

---
+++ step3/RxSwift+MVVM/Pages/Menu.swift
@@ -0,0 +1,16 @@
+//
+//  Menu.swift
+//  RxSwift+MVVM
+//
+//  Created by 임현규 on 2022/11/03.
+//  Copyright © 2022 iamchiwon. All rights reserved.
+//
+
+import Foundation
+
+// ViewModel
+struct Menu {
+    var name: String
+    var price: Int
+    var count: Int
+}
---
+++ step3/RxSwift+MVVM/Pages/MenuList/MenuListViewModel.swift
@@ -0,0 +1,20 @@
+//
+//  MenuListViewModel.swift
+//  RxSwift+MVVM
+//
+//  Created by 임현규 on 2022/11/03.
+//  Copyright © 2022 iamchiwon. All rights reserved.
+//
+
+import Foundation
+
+class MenuListViewModel {
+    
+    let menus: [Menu] = [
+        Menu(name: "튀김1", price: 100, count: 0),
+        Menu(name: "튀김2", price: 200, count: 0),
+        Menu(name: "튀김3", price: 300, count: 0),
+        Menu(name: "튀김4", price: 400, count: 0),
+        Menu(name: "튀김5", price: 500, count: 0),
+    ]
+}
